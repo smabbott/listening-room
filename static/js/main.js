154506555ea3007e5b41cec9bcde5dbf9b021276
Grifola <seth.mabbott@gmail.com>
2026-07-22 14:24:58 +0200

    ping with linear ramp as envelope

--- static/js/main.js
+++ static/js/main.js
@@ -11,20 +11,38 @@ addEventListener("DOMContentLoaded", (event) => {
   //    - voice types
   //
   const context = new AudioContext();
-
   const osc = context.createOscillator();
+  const gain = context.createGain();
+
   osc.type="square";
   osc.frequency.setValueAtTime(440, context.currentTime);
-  osc.connect(context.destination);
+  
+  
 
   const startButton = document.querySelector(".start");
   startButton.addEventListener("click", startAudio);
 
+  const stopButton = document.querySelector(".stop");
+  stopButton.addEventListener("click", stopAudio);
+
   function startAudio(){
     osc.start();
+    osc.connect(gain);
+    gain.connect(context.destination);
+    const now = context.currentTime;
+    gain.gain.setValueAtTime(0, now);
+    gain.gain.linearRampToValueAtTime(1, now + 0.01);
+    gain.gain.linearRampToValueAtTime(0.3, now+0.05);
+    gain.gain.linearRampToValueAtTime(0, now + 0.5);
+  }
+
+  function stopAudio(){
+    osc.stop();
   }
 
 });
+
+
 
 
 
