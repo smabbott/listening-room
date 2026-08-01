c87fd8ad1202c665164ce7612ae5d2b4fa3b3e7d
Grifola <seth.mabbott@gmail.com>
2026-08-01 20:14:22 +0200

    web sockets passing data back and forth

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 
 SITE_NAME = "Listening Room"
 AUTHOR = "Seth Mabbott"
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "change_this"
 
 socketio = SocketIO(app)
+
+# keep track of details that define a room. 
+# It's ok if this object is not persistant since it is updated any time a client connects or disconnects
+voices = {}
+users = {}
+
 # ROUTES #
 
 # Home
 @app.route("/")
-def home():
+def index():
     return render_template("rooms/1.html")
 
 # SOCKETIO ROUTES #
-@socketio.on("connect")
-def handle_connect():
-    print("connected")
+# @socketio.on("connect")
+# def handle_connect(d):
+    # TODO: what information do we get about the client here?
+    # how might those map to a voice on the front end?
     # When a client connects, broadcast a "room state object" to all clients describing parameters for the room
+# socketio.emit("state_update",  room_status)
+
+# FIXME: if the client refreshes there is a connection error
+@socketio.on('join')
+def handle_join(d):
+    emit("add_voice", d)
+
 
 @socketio.on("message")
 def handle_message(msg):
